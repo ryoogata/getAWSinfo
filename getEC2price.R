0ccad28b9e5da73c79ef931cbfe4fe1f48032aa8
ryoogata <ryo.ogata@gmail.com>
2015-09-18 23:52:29 +0900

    list からの情報取得方法記述方法の変更

--- getEC2price.R
+++ getEC2price.R
@@ -11,17 +11,17 @@ pricelist <- list()
 # listcount の初期値を設定
 listcount <- 1
       
-for ( i_regions in 1:length(jsondata[[2]]$regions))
+for ( i_regions in 1:length(jsondata$config$regions))
 {
-  for ( i_instanceTypes in 1:length(jsondata[[2]]$regions[[i_regions]]$instanceTypes))
+  for ( i_instanceTypes in 1:length(jsondata$config$regions[[i_regions]]$instanceTypes))
   {
-    for ( i_sizes in 1:length(jsondata[[2]]$regions[[i_regions]]$instanceTypes[[i_instanceTypes]]$sizes))
+    for ( i_sizes in 1:length(jsondata$config$regions[[i_regions]]$instanceTypes[[i_instanceTypes]]$sizes))
     {
-      region <- jsondata[[2]]$regions[[i_regions]]$region
-      type <- jsondata[[2]]$regions[[i_regions]]$instanceTypes[[i_instanceTypes]]$type
-      size <- jsondata[[2]]$regions[[i_regions]]$instanceTypes[[i_instanceTypes]]$sizes[[i_sizes]]$size
-      name <- jsondata[[2]]$regions[[i_regions]]$instanceTypes[[i_instanceTypes]]$sizes[[i_sizes]]$valueColumns[[1]]$name
-      USD <- jsondata[[2]]$regions[[i_regions]]$instanceTypes[[i_instanceTypes]]$sizes[[i_sizes]]$valueColumns[[1]]$prices$USD
+      region <- jsondata$config$regions[[i_regions]]$region
+      type <- jsondata$config$regions[[i_regions]]$instanceTypes[[i_instanceTypes]]$type
+      size <- jsondata$config$regions[[i_regions]]$instanceTypes[[i_instanceTypes]]$sizes[[i_sizes]]$size
+      name <- jsondata$config$regions[[i_regions]]$instanceTypes[[i_instanceTypes]]$sizes[[i_sizes]]$valueColumns[[1]]$name
+      USD <- jsondata$config$regions[[i_regions]]$instanceTypes[[i_instanceTypes]]$sizes[[i_sizes]]$valueColumns[[1]]$prices$USD
       pricelist[[listcount]] <- c(region, type, size, name, USD)
       listcount <- listcount + 1
     }
