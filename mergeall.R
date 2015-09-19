424e271dd38462a4cc7d2c5008d3b19c165a3f82
ryoogata <ryo.ogata@gmail.com>
2015-09-20 01:04:55 +0900

    コメントを追記

--- mergeall.R
+++ mergeall.R
@@ -1,7 +1,14 @@
+# getEC2info.R スクリプト実行
 source("getEC2info.R")
+
+# getEC2price.R スクリプト実行
 source("getEC2price.R")
 
+# 東京リージョン情報収集
 tokyoprice <- priceDF[priceDF$region == "ap-northeast-1",c("size", "USD")]
 
+# 全リージョンの価格情報を含めて merge
 alltable <- merge( x = table, y = priceDF, by.x = "InstanceType", by.y = "size")
+
+# 東京リージョンのみ価格情報を含めて merge
 tokyotable <- merge( x = table, y = tokyoprice, by.x = "InstanceType", by.y = "size")
